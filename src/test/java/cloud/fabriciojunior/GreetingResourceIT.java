557290237739eb9d332e81a413b07238abb6c5c7
quarkusio <no-reply@quarkus.io>
2024-10-13 14:45:37 +0000

    Initial commit

---
+++ src/test/java/cloud/fabriciojunior/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package cloud.fabriciojunior;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
